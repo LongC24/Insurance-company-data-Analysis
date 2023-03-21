607fe73fe449ab6d2d97b7132714febefb0204a5
LongC24 <chrisday1224@outlook.com>
2023-03-21 12:06:04 -0700

    更新处理NLG文件处理 更新注释

--- readNLG_INFORCE.py
+++ readNLG_INFORCE.py
@@ -1,67 +1,53 @@
-# 然后删除最后至仍有数据的行
-
 # 文件预先处理后数据正确后 导出至CSV  然后导入数据库
 import pandas as pd
-import numpy as np
 import os
-import glob
 import re
 
 
-#  查找importFile文件夹下的文件 并获取文件名为NLG 开头的文件名
+def get_file_name(path):  # 获取文件夹下的文件名
+    files_names = os.listdir(path)
+    return files_names
+
+
+def process_nlg_new(data_frame):  # excel 进行处理
+    # Read the Excel file
+    data_frame = pd.read_excel(inputFile)
+    print(data_frame[0:5])
+
+    # 先删除前5行至标题行
+    data_frame = data_frame.drop(data_frame.index[0:4])
+    print(data_frame[0:5])
+    data_frame.columns = data_frame.iloc[0]
+    print(data_frame[0:5])
+
+    # 删除倒数三行
+    data_frame = data_frame.drop(data_frame.index[-3:])
+    # 删除第一行
+    data_frame = data_frame.drop(data_frame.index[0])
+
+    return data_frame
 
 
 # 获取文件夹下的文件名
-def getFileName(path):
-    files = os.listdir(path)
-    return files
-
-
-# 获取文件夹下的文件名
-files = getFileName('importFile')
-print(files)
+files_name = get_file_name('importFile')
 # 文件名为NLG 开头的文件名
-for file in files:
+for file in files_name:
     if re.match(r'NLG_In', file):
         fileName = file
 
-# 文件名
-# fileName = 'NLG_AllNewBusinessReport_03072023'
-
+# 设置文件名
 inputFile = 'importFile/' + fileName
 outputFile = 'exportFile/' + fileName + '.csv'
 
 # Read the Excel file
 df = pd.read_excel(inputFile)
-print(df[0:5])
 
-# 先删除前5行至标题行
-df = df.drop(df.index[0:4])
-print(df[0:5])
-df.columns = df.iloc[0]
-print(df[0:5])
-
-# 删除倒数三行
-df = df.drop(df.index[-3:])
-# 删除第一行
-df = df.drop(df.index[0])
-
-# df = df.reindex(df.drop(df.index[0:0]))
-
-# 设置第一行为标题行
-
-
-# 重新排序
-
-
-print(df)
-
-# for i in df:
-#     print(df[i])
+# excel 进行处理
+df = process_nlg_new(df)
 
 # set the row Submitted data type to date
+# 转换日期格式 方便插入数据库
 df['Issue Date'] = pd.to_datetime(df['Issue Date'])
 
 # Save the Excel file to  CSV
-
 df.to_csv(outputFile, index=False)
--- readNLG_NEW.py
+++ readNLG_NEW.py
@@ -12,8 +12,7 @@ def get_file_name(path):  # 获取文件夹下的文件名
     return files_names
 
 
-def process_NLG_NEW(data_frame):  # excel 进行处理
-
+def process_nlg_new(data_frame):  # excel 进行处理
     # 先删除前5行至标题行
     data_frame = data_frame.drop(data_frame.index[0:4])
     # 设置第一行为标题行
@@ -26,43 +25,25 @@ def process_NLG_NEW(data_frame):  # excel 进行处理
 
 
 # 获取文件夹下的文件名
-files = get_file_name('importFile')
-print(files)
+files_name = get_file_name('importFile')
 # 文件名为NLG 开头的文件名
-for file in files:
+for file in files_name:
     if re.match(r'NLG_All', file):
         fileName = file
-
-# 文件名
-# fileName = 'NLG_AllNewBusinessReport_03072023'
-
 
 # 设置文件名
 inputFile = 'importFile/' + fileName
 outputFile = 'exportFile/' + fileName + '.csv'
+
 # Read the Excel file
 df = pd.read_excel(inputFile)
 
-
 # excel 进行处理
-def process_nlg_new(dataFram):
-    # 先删除前5行至标题行
-    dataFram = dataFram.drop(dataFram.index[0:4])
-    # 设置第一行为标题行
-    dataFram.columns = dataFram.iloc[0]
-    # 删除倒数5行
-    dataFram = dataFram.drop(dataFram.index[-5:])
-    # 删除第一行 第一行设置成标题后 会有重复
-    dataFram = dataFram.drop(dataFram.index[0])
-    return dataFram
-
-
-# df = df.reindex(df.drop(df.index[0:0]))
-
+df = process_nlg_new(df)
 
 # set the row Submitted data type to date
+# 转换日期格式 方便插入数据库
 df['Submitted'] = pd.to_datetime(df['Submitted'])
 
 # Save the Excel file to  CSV
-
 df.to_csv(outputFile, index=False)
